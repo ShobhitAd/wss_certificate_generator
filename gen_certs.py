3760a34f604e70308a173f4f26939d478e127c77
Shobhit Adlakha <adlakhashobhit@gmail.com>
2019-04-15 11:40:19 -0400

    Moved shell commands to different class

--- gen_certs.py
+++ gen_certs.py
@@ -5,11 +5,67 @@ import subprocess
 import pexpect
 # from pprint import pprint
 
+class CertCommands():
+  def gen_key(self, key_file):
+    # openssl genrsa -out [KEY] 4096
+    gen_command = "openssl genrsa -out %s 4096" % (key_file)
+    subprocess.call(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+
+  def gen_cert(self, key_file, cert_file, inputs):
+    self.check_inputs('GENERATE CERTIFICATE', inputs, required=['ip'])
+    #openssl req -new -x509 -key [KEY] -out [CERTIFICATE] -days 1095
+    gen_command = "openssl req -new -x509 -key %s -out %s -days 1095" % (key_file, cert_file)
+    gen_process = subprocess.Popen(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    gen_input='.\n.\n.\n.\n.\n%s\n.\n' % inputs['ip']
+    gen_process.communicate(input=bytes(gen_input, 'utf-8'))
+    gen_process.wait()
+
+  def gen_key_and_cert(self, key_file, cert_file, inputs):
+    self.check_inputs('GENERATE KEY AND CERTIFICATE', inputs, required=['ip'])
+    # openssl req -newkey rsa:2048 -nodes -keyout [KEY] -x509 -days 365 -out [CERTIFICATE]
+    gen_command = "openssl req -newkey rsa:2048 -nodes -keyout %s -x509 -days 1095 -out %s" % (key_file, cert_file)
+    gen_process = subprocess.Popen(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    gen_input='.\n.\n.\n.\n.\n%s\n.\n' % inputs['ip']
+    gen_process.communicate(input=bytes(gen_input, 'utf-8'))
+    gen_process.wait()
+
+  def gen_keystore(self, key_file, cert_file, keystore_file, inputs):
+    self.check_inputs('GENERATE KEYSTORE', inputs, required=['keystore_password'])
+    #openssl pkcs12 -export -out [KEYSTORE] -inkey [KEY] -in [CERT]
+    gen_process = pexpect.spawn("openssl pkcs12 -export -out %s -inkey %s -in %s" % (keystore_file, key_file, cert_file))
+    gen_process.expect("Enter Export Password")
+    gen_process.sendline(inputs['keystore_password'])
+    gen_process.expect("Verifying - Enter Export Password")
+    gen_process.sendline(inputs['keystore_password'])
+    gen_process.expect(pexpect.EOF)
+
+  def gen_jks(self, keystore_file, jks_file, inputs):
+    self.check_inputs('GENERATE JAVA KEYSTORE', inputs, required=['jks_password', 'keystore_password'])
+    # keytool -importkeystore -destkeystore [JKS] -srcstoretype PKCS12 -srckeystore [KEYSTORE]
+    gen_command = "keytool -importkeystore -destkeystore %s -srcstoretype PKCS12 -srckeystore %s" % (jks_file, keystore_file)
+    gen_process = subprocess.Popen(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    gen_input='%s\n%s\n%s\n' % (inputs['jks_password'], inputs['jks_password'], inputs['keystore_password'])
+    gen_process.communicate(input=bytes(gen_input, 'utf-8'))
+    gen_process.wait()
+
+  def check_inputs(self, tag, inputs, required):
+    for arg in required:
+      if arg not in inputs:
+        print("%s: Missing input argument(%s)" % (tag, arg))
+        exit(1)
+
 class CertManager():
     def __init__(self, params):
         self.PARAMS = params
         self.KEYSTORE_PASSWORD = 'default_password'
         self.JKS_PASSWORD = 'default_password'
+        self.COMMANDS = CertCommands()
+        self.DEFAULT_FILENAMES = {
+          'key' : 'privatekey.key',
+          'cert' : 'cert.pem',
+          'keystore': 'keystore.p12',
+          'jks': 'jkeystore.jks'
+        }
         
     def generate(self):
         # Validate file args
@@ -28,23 +84,6 @@ class CertManager():
         self.gen_cert()
         self.gen_keystore()
         self.gen_jks()
-        
-    def gen_key_and_cert(self):
-        if self.some_req('keystore', 'key', 'cert'):
-            return
-        if not self.all_req('ip'):
-            print('Missing required parameters')     
-            exit(1)   
-        print('Generating private key and cert')
-        # openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
-        gen_command = "openssl req -newkey rsa:2048 -nodes -keyout privatekey.key -x509 -days 1095 -out cert.pem"
-        gen_process = subprocess.Popen(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-        gen_input='.\n.\n.\n.\n.\n%s\n.\n' % self.PARAMS['ip']
-        gen_process.communicate(input=bytes(gen_input, 'utf-8'))
-        gen_process.wait()
-
-        self.PARAMS['key'] = 'privatekey.key'
-        self.PARAMS['cert'] = 'cert.pem'
 
     def gen_cert(self):
         if self.some_req('keystore', 'cert'):
@@ -54,14 +93,22 @@ class CertManager():
             exit(1)   
 
         print('Generating cert based on private key')
-        #openssl req -new -x509 -key key.pem -out cacert.pem -days 1095
-        gen_command = "openssl req -new -x509 -key %s -out cert.pem -days 1095" % self.PARAMS['key']
-        gen_process = subprocess.Popen(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-        gen_input='.\n.\n.\n.\n.\n%s\n.\n' % self.PARAMS['ip']
-        gen_process.communicate(input=bytes(gen_input, 'utf-8'))
-        gen_process.wait()
+        self.COMMANDS.gen_cert(self.PARAMS['key'], self.DEFAULT_FILENAMES['cert'], {'ip': self.PARAMS['ip']} )
 
-        self.PARAMS['cert'] = 'cert.pem'
+        self.PARAMS['cert'] = self.DEFAULT_FILENAMES['cert']
+
+    def gen_key_and_cert(self):
+        if self.some_req('keystore', 'key', 'cert'):
+            return
+        if not self.all_req('ip'):
+            print('Missing required parameters')     
+            exit(1)      
+
+        print('Generating private key and cert')
+        self.COMMANDS.gen_key_and_cert(self.DEFAULT_FILENAMES['key'], self.DEFAULT_FILENAMES['cert'], {'ip': self.PARAMS['ip']} )
+
+        self.PARAMS['key'] = self.DEFAULT_FILENAMES['key']
+        self.PARAMS['cert'] = self.DEFAULT_FILENAMES['cert']
 
     def gen_keystore(self):
         if self.some_req('keystore'):
@@ -71,34 +118,22 @@ class CertManager():
             exit(1)   
 
         print('Generating keystore based on cert and private key')
-        #openssl pkcs12 -export -out keystore.p12 -inkey privatekey.key -in cert.pem
-        gen_process = pexpect.spawn("openssl pkcs12 -export -out keystore.p12 -inkey %s -in %s" % (self.PARAMS['key'], self.PARAMS['cert']))
-        gen_process.expect("Enter Export Password")
-        gen_process.sendline(self.KEYSTORE_PASSWORD)
-        gen_process.expect("Verifying - Enter Export Password")
-        gen_process.sendline(self.KEYSTORE_PASSWORD)
-        gen_process.expect(pexpect.EOF)
+        self.COMMANDS.gen_keystore(self.PARAMS['key'], self.PARAMS['cert'], self.DEFAULT_FILENAMES['keystore'], {'keystore_password': self.KEYSTORE_PASSWORD} )
 
-        self.PARAMS['keystore'] = 'keystore.p12'
+        self.PARAMS['keystore'] = self.DEFAULT_FILENAMES['keystore']
     
     def gen_jks(self):
         if self.some_req('jks'):
             return
         if not self.all_req('keystore'):
             print('Missing required parameters')     
-            exit(1)   
+            exit(1)
 
         print('Generating JKS based on keystore')
-        # keytool -importkeystore -destkeystore keystore.jks -srcstoretype PKCS12 -srckeystore keystore.p12
-        gen_command = "keytool -importkeystore -destkeystore jkeystore.jks -srcstoretype PKCS12 -srckeystore %s" % (self.PARAMS['keystore'])
-        gen_process = subprocess.Popen(gen_command.split(' '), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-        gen_input='%s\n%s\n%s\n' % (self.JKS_PASSWORD, self.JKS_PASSWORD, self.KEYSTORE_PASSWORD)
-        gen_process.communicate(input=bytes(gen_input, 'utf-8'))
-        gen_process.wait()
+        self.COMMANDS.gen_jks(self.PARAMS['keystore'], self.DEFAULT_FILENAMES['jks'], {'keystore_password': self.KEYSTORE_PASSWORD, 'jks_password': self.JKS_PASSWORD} )
 
-        # print(out[0].decode())
+        self.PARAMS['jks'] = self.DEFAULT_FILENAMES['jks']
 
-        self.PARAMS['jks'] = 'jkeystore.jks'
     def all_req(self, *flags):
         res = True
         for f in flags:
